d5c6fafe97f04b1e6c2f2bfb479e72306933177a
Manuel Zametter <m.zametter@gmail.com>
2023-11-02 11:51:00 +0100

    Custom classloader example

--- reverse-engineering-java/1-hello-world/src/main/java/org/se/lab/HelloWorld.java
+++ reverse-engineering-java/1-hello-world/src/main/java/org/se/lab/HelloWorld.java
@@ -1,11 +1,8 @@
 package org.se.lab;
 
-public class HelloWorld
-{
-	public static void main(String... args)
-	{
-		for(int i=0; i<10; i++)
-		{
+public class HelloWorld {
+	public static void main(String... args) {
+		for (int i = 0; i < 10; i++) {
 			System.out.println("Hello World!");
 		}
 	}
---
+++ reverse-engineering-java/3-custom-class-loader/src/classes/HelloWorld.java
@@ -0,0 +1,7 @@
+package org.se.lab;
+
+public class HelloWorld {
+    public void sayHello() {
+        System.out.println("Hello KBerg!!!");
+    }
+}
